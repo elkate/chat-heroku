ed1d71b07b8230d8e73b1e9096e1ce99403a76da
EkaterinaEliseeva <katerina17778@gmail.com>
2020-01-20 14:39:12 +0300

    add

---
+++ frontend/build/precache-manifest.d31d750875c8ec5d24d352d0d0707a87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de165ffd18c070f11433b34e465ccdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d125ccf553755e9bf46",
+    "url": "/static/css/main.4d33ecf9.chunk.css"
+  },
+  {
+    "revision": "0393f35f7496631b7cf6",
+    "url": "/static/js/2.5c15cce8.chunk.js"
+  },
+  {
+    "revision": "4d125ccf553755e9bf46",
+    "url": "/static/js/main.1a7a9195.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
